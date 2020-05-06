f5d89b4510b7df53c5c92a58466c4c3f7c9acbd9
pradanahafiidh <pradanahafidh@gmail.com>
2020-05-06 20:49:08 +0700

    Class anak(derivedClass)

---
+++ Epson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorpishm
+{
+    class Epson : PrinterWindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+        public override void print()
+        {
+            Console.WriteLine("Epson printer printing....");
+        }
+
+    }
+}
---
+++ LaserJet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorpishm
+{
+    class LaserJet : PrinterWindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("LaserJet display dimension : 12*12");
+        }
+        public override void print()
+        {
+            Console.WriteLine("LaserJet printer printing....");
+        }
+    }
+}
