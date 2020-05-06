aaf879eabfd5f8f619528c7207f7a83b75c316b6
pradanahafiidh <pradanahafidh@gmail.com>
2020-05-06 20:41:24 +0700

    Class induk(base class)

---
+++ PrinterWindows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorpishm
+{
+    class PrinterWindows
+    {
+        public string namaPrinter { get; set; }
+        public virtual void show()
+        {
+            Console.WriteLine("ini printer");
+        }
+        public virtual void print()
+        {
+            Console.WriteLine("printer printing....");
+        }
+    }
+}
